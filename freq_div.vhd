c27652d65b6c45fff478ba5e40b63db7ef58068f
GeoMassalas <gmassalas@gmail.com>
2017-12-15 19:25:01 +0200

    changes to complement last change

--- freq_div.vhd
+++ freq_div.vhd
@@ -1,35 +1,34 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_unsigned.ALL;
+
 entity freq_div is
 	port( clk50 : in STD_LOGIC;
 			clks : out STD_LOGIC_VECTOR(3 downto 0));
 end freq_div;
 
 architecture Behavioral of freq_div is
-component freq_div_to_1
+
+	component freq_div_u
+		generic (N:positive:=1);
 		port(clk50 : in STD_LOGIC;
-				clk1 : out STD_LOGIC);
-end component;
-component freq_div_to_10
-		port(clk50 : in STD_LOGIC;
-				clks10 : out STD_LOGIC);
-end component;
-component freq_div_to_100
-		port(clk50 : in STD_LOGIC;
-				clks100 : out STD_LOGIC);
-end component;
-component freq_div_to_1000
-		port(clk50 : in STD_LOGIC;
-				clk1000 : out STD_LOGIC);
-end component;
+					clk : out STD_LOGIC);
+	end component;
+
+	signal O : STD_LOGIC_VECTOR(3 downto 0);
 begin
-FD0:	freq_div_to_1
-	port map(clk50,clks(0));
-FD1:	freq_div_to_10
-	port map(clk50,clks(1));
-FD2:	freq_div_to_100
-	port map(clk50,clks(2));
-FD3:	freq_div_to_1000
-	port map(clk50,clks(3));
-
-
+	FD0:	freq_div_u 
+		generic map(N=>25000000)
+		port map(clk50,O(0)); -- 1hz
+	FD1:	freq_div_u 
+		generic map(N=>2500000)
+		port map(clk50,O(1)); -- 10 hz
+	FD2:	freq_div_u
+		generic map(N=>250000)
+		port map(clk50,O(2)); -- 100 hz
+	FD3:	freq_div_u
+		generic map(N=>25000)
+		port map(clk50,O(3)); -- 1 Khz
+	clks <= O;
 end Behavioral;
 
