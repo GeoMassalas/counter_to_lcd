13fd9911eb5e186b4f2c4b10e01ebbac6e8039a4
GeoMassalas <gmassalas@gmail.com>
2017-12-16 22:06:52 +0200

    fixed bugs in divider adding simulation and png

--- divider.vhd
+++ divider.vhd
@@ -1,6 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
 
 
 entity divider is
@@ -10,31 +10,31 @@ end divider;
 
 architecture Behavioral of divider is
 -- Signals
-	signal n,remainder : integer;
 	signal temp1,temp2,temp3,temp4,temp5 : integer;
 begin
-	div_process : process
+	div_process : process(input)
+	variable n0,n1,n2,n3 : integer;
+
 	begin
-		n <= to_integer(unsigned(input));
+		n0 := conv_integer(unsigned(input));
 		-- 1st digit
-		temp1 <= n mod 10000;
-		n <= n rem 10000;
+		temp1 <= n0 / 10000;
+		n1 := n0 rem 10000;
 		-- 2nd digit
-		temp2 <= n mod 1000;
-		n <= n rem 1000;
+		temp2 <= n1 / 1000;
+		n2 := n1 rem 1000;
 		-- 3rd digit
-		temp3 <= n mod 100;
-		n <= n rem 100;
+		temp3 <= n2 / 100;
+		n3 := n2 rem 100;
 		-- 4th digit
-		temp4 <= n mod 10;
-		temp5 <= n rem 10;
+		temp4 <= n3 / 10;
+		temp5 <= n3 rem 10;
 	end process;
-	
-	d1 <= std_logic_vector(to_unsigned(temp1, d1'length));
-	d2 <= std_logic_vector(to_unsigned(temp2, d2'length));
-	d3 <= std_logic_vector(to_unsigned(temp3, d3'length));
-	d4 <= std_logic_vector(to_unsigned(temp4, d4'length));
-	d5 <= std_logic_vector(to_unsigned(temp5, d5'length));
+	d1 <= conv_std_logic_vector(temp1, d1'length);
+	d2 <= conv_std_logic_vector(temp2, d2'length);
+	d3 <= conv_std_logic_vector(temp3, d3'length);
+	d4 <= conv_std_logic_vector(temp4, d4'length);
+	d5 <= conv_std_logic_vector(temp5, d5'length);
 	
 end Behavioral;
 
---
+++ divider_tb.vhd
@@ -0,0 +1,65 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.std_logic_unsigned.ALL;
+ 
+ENTITY divider_tb IS
+END divider_tb;
+ 
+ARCHITECTURE behavior OF divider_tb IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT divider
+    PORT(
+         input : IN  std_logic_vector(15 downto 0);
+         d5 : OUT  std_logic_vector(3 downto 0);
+         d1 : OUT  std_logic_vector(3 downto 0);
+         d2 : OUT  std_logic_vector(3 downto 0);
+         d3 : OUT  std_logic_vector(3 downto 0);
+         d4 : OUT  std_logic_vector(3 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal input : std_logic_vector(15 downto 0) := (others => '0');
+
+ 	--Outputs
+   signal d5 : std_logic_vector(3 downto 0);
+   signal d1 : std_logic_vector(3 downto 0);
+   signal d2 : std_logic_vector(3 downto 0);
+   signal d3 : std_logic_vector(3 downto 0);
+   signal d4 : std_logic_vector(3 downto 0);
+
+	-- Clock 
+   constant clk_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: divider PORT MAP (
+          input => input,
+          d5 => d5,
+          d1 => d1,
+          d2 => d2,
+          d3 => d3,
+          d4 => d4
+        );
+
+   -- Clock process definitions
+   clk_process :process
+   begin
+		input <= input + '1';
+		wait for clk_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin	
+      -- insert stimulus here 
+
+      wait;
+   end process;
+
+END;
