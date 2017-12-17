15e6fa3902d97722d89957f9214ff3e5357a6495
GeoMassalas <gmassalas@gmail.com>
2017-12-17 21:07:57 +0200

    changes added

--- divider.vhd
+++ divider.vhd
@@ -1,7 +1,5 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-
 
 entity divider is
 	port(input : in STD_LOGIC_VECTOR(15 downto 0);
@@ -9,32 +7,32 @@ entity divider is
 end divider;
 
 architecture Behavioral of divider is
--- Signals
-	signal temp1,temp2,temp3,temp4,temp5 : integer;
+component alt_divider
+	generic(divisor :integer := 10);
+	port(divident : in STD_LOGIC_VECTOR(15 downto 0);
+			quotient : out STD_LOGIC_VECTOR(3 downto 0);
+			remainder : out STD_LOGIC_VECTOR(15 downto 0));
+end component;
+signal q1,q2,q3,q4 : STD_LOGIC_VECTOR(3 downto 0);
+signal r1,r2,r3,r4 : STD_LOGIC_VECTOR(15 downto 0);
 begin
-	div_process : process(input)
-	-- Variables
-	variable n0,n1,n2,n3 : integer;
-	begin
-		n0 := conv_integer(unsigned(input));
-		-- 1st digit
-		temp1 <= n0 / 10000;
-		n1 := n0 rem 10000;
-		-- 2nd digit
-		temp2 <= n1 / 1000;
-		n2 := n1 rem 1000;
-		-- 3rd digit
-		temp3 <= n2 / 100;
-		n3 := n2 rem 100;
-		-- 4th digit
-		temp4 <= n3 / 10;
-		temp5 <= n3 rem 10;
-	end process;
-	d1 <= conv_std_logic_vector(temp1, d1'length);
-	d2 <= conv_std_logic_vector(temp2, d2'length);
-	d3 <= conv_std_logic_vector(temp3, d3'length);
-	d4 <= conv_std_logic_vector(temp4, d4'length);
-	d5 <= conv_std_logic_vector(temp5, d5'length);
+	DIV1 : alt_divider 
+		generic map( divisor => 10000)
+		port map(divident => input, quotient => q1, remainder => r1);
+	DIV2 : alt_divider 
+		generic map( divisor => 1000)
+		port map(divident => r1, quotient => q2, remainder => r2);
+	DIV3 : alt_divider 
+		generic map( divisor => 100)
+		port map(divident => r2, quotient => q3, remainder => r3);
+	DIV4 : alt_divider 
+		generic map( divisor => 10)
+		port map(divident => r3, quotient => q4, remainder => r4);
+		d1 <= q1;
+		d2 <= q2;
+		d3 <= q3;
+		d4 <= q4;
+		d5 <= r4(3 downto 0);
 	
 end Behavioral;
 
