f935b86eade90e406eba28e60b71304227199f3a
GeoMassalas <gmassalas@gmail.com>
2018-01-09 00:41:42 +0200

    switched the alt-divider

--- alt_divider.vhd
+++ alt_divider.vhd
@@ -1,9 +1,10 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity alt_divider is
-	generic(divisor :integer := 10);
+	generic(divisor : integer := 10);
 	port(divident : in STD_LOGIC_VECTOR(15 downto 0);
 			quotient : out STD_LOGIC_VECTOR(3 downto 0);
 			remainder : out STD_LOGIC_VECTOR(15 downto 0));
@@ -14,19 +15,21 @@ signal q : STD_LOGIC_VECTOR(3 downto 0);
 signal r : STD_LOGIC_VECTOR(15 downto 0);
 begin
 	div_process : process(divident)
-		variable n : integer;
-		variable temp : integer;
+		variable n : UNSIGNED(15 downto 0);
+		variable t : STD_LOGIC_VECTOR(3 downto 0);
+		variable z : UNSIGNED(15 downto 0);
 	begin
-		temp := 0;
-		n := to_integer(unsigned(divident));
+		z := to_unsigned(divisor, z'length);
+		t := "0000";
+		n := unsigned(divident);
 		for i in 0 to 9 loop
-			if n >= divisor then
-				temp := temp + 1;
-				n := n - divisor;
+			if n >= z then
+				n := n - z;
+				t := t + '1';
 			end if;
 		end loop;
-		q <= std_logic_vector(to_unsigned(temp, quotient'length));
-		r <= std_logic_vector(to_unsigned(n, remainder'length));
+		q <= t;
+		r <= std_logic_vector(n);
 	end process;
 	quotient <= q;
 	remainder <= r;
